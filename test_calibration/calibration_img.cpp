f29b141106e3c7879c348fcdf7ff9ffa0a6f30a8
WY-Huang <1319804977@qq.com>
2023-09-27 12:50:49 +0800

    1

--- test_calibration/calibration_img.cpp
+++ test_calibration/calibration_img.cpp
@@ -6,7 +6,7 @@ namespace fs = boost::filesystem;
 int main()
 {
     // 批量读取文件
-    std::string folderPath = "/home/wanyel/contours/halcon_calibration/20230916-17-45-cab/rawImgs";
+    std::string folderPath = "/home/wanyel/contours/halcon_calibration/20230919/rawImgs";
 
     fs::path directory(folderPath);
 
@@ -32,35 +32,38 @@ int main()
             cv::Mat srcimg = cv::imread(imgPath, 0);
 
             // 灰度变换
-            cv::Mat grayTransImg = srcimg.clone();
-            for (int i = 0; i < srcimg.rows; i++)
-            {
-                for (int j = 0; j < srcimg.cols; j++)
-                {
-                    grayTransImg.at<uchar>(i, j) = 2 * log((double)(grayTransImg.at<uchar>(i, j)) + 1) / log(100);  //对数变换 s=6*log(r+1)
-                }
-            }
-            cv::normalize(grayTransImg, grayTransImg, 0, 255, cv::NORM_MINMAX);  //图像归一化，转到0~255范围内
-            cv::convertScaleAbs(grayTransImg, grayTransImg);  //数据类型转换到CV_8U
+            // cv::Mat grayTransImg = srcimg.clone();
+            // for (int i = 0; i < srcimg.rows; i++)
+            // {
+            //     for (int j = 0; j < srcimg.cols; j++)
+            //     {
+            //         grayTransImg.at<uchar>(i, j) = 2 * log((double)(grayTransImg.at<uchar>(i, j)) + 1) / log(100);  //对数变换 s=6*log(r+1)
+            //     }
+            // }
+            // cv::normalize(grayTransImg, grayTransImg, 0, 255, cv::NORM_MINMAX);  //图像归一化，转到0~255范围内
+            // cv::convertScaleAbs(grayTransImg, grayTransImg);  //数据类型转换到CV_8U
+
 
             // 高斯滤波
             // cv::Mat gaussImg;
             // cv::GaussianBlur(srcimg, gaussImg, cv::Size(0, 0), 3, 3);
 
+
             // 二值化
-            // cv::Mat binaryImg;
-            // cv::threshold(srcimg, binaryImg, 80, 255, cv::THRESH_BINARY);
+            cv::Mat binaryImg;
+            cv::threshold(srcimg, binaryImg, 80, 255, cv::THRESH_BINARY);
+
 
             // 重设置图像大小
-            cv::resize(grayTransImg, grayTransImg, cv::Size(1536, 1024));
+            // cv::resize(srcimg, srcimg, cv::Size(1536, 1024));
             // cv::imshow("grayTransImg", grayTransImg);
             // cv::waitKey(0);
 
             // 重命名图像路径
             fs::path path_temp(filePath);
-            std::string stem = "/home/wanyel/contours/halcon_calibration/20230916-17-45-cab/grayTransImgs_1536/" + path_temp.stem().string() + "_1536.jpg";
+            std::string stem = "/home/wanyel/contours/halcon_calibration/20230919/binaryImgs/" + path_temp.stem().string() + "_binary.jpg";
             std::cout << stem << std::endl;
-            cv::imwrite(stem, grayTransImg);
+            cv::imwrite(stem, binaryImg);
 
         }
     }
